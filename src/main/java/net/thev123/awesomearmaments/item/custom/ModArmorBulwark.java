382c7b5abda68e78a1c1c6cbc28d02ee04a2d0cb
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-07-08 17:35:09 -0400

    added attributes and effects to bulwark armor

--- src/main/java/net/thev123/awesomearmaments/item/custom/ModArmorBulwark.java
+++ src/main/java/net/thev123/awesomearmaments/item/custom/ModArmorBulwark.java
@@ -1,10 +1,123 @@
 package net.thev123.awesomearmaments.item.custom;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.thev123.awesomearmaments.item.ModArmorMaterials;
 
-public class ModArmorBulwark extends ArmorItem {
+import java.util.Map;
+
+public class ModArmorBulwark extends ArmorItem{
+    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
     public ModArmorBulwark(ArmorMaterial material, Type type, Settings settings) {
         super(material, type, settings);
+
+        //attributes
+        int protection = ModArmorMaterials.BULWARK.getProtection(type);
+        float toughness = ModArmorMaterials.BULWARK.getToughness();
+        float knockbackResistance = ModArmorMaterials.BULWARK.getKnockbackResistance();
+
+        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier("Speed  modifier", -0.01, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ARMOR, new EntityAttributeModifier("Armor  modifier", protection, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, new EntityAttributeModifier("Armor  toughness", toughness, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, new EntityAttributeModifier("Armor knockback resistance", knockbackResistance, EntityAttributeModifier.Operation.ADDITION));
+
+        this.attributeModifiers = builder.build();
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        if (slot == this.type.getEquipmentSlot()) {
+            return this.attributeModifiers;
+        }
+        return super.getAttributeModifiers(slot);
+    }
+
+    //effects
+    private static final Map<ArmorMaterial, StatusEffectInstance> MATERIAL_TO_EFFECT_MAP1 =
+            new ImmutableMap.Builder<ArmorMaterial, StatusEffectInstance>()
+                    .put(ModArmorMaterials.BULWARK, new StatusEffectInstance(StatusEffects.RESISTANCE, 200, 0))
+                    .build();
+    private static final Map<ArmorMaterial, StatusEffectInstance> MATERIAL_TO_EFFECT_MAP2 =
+            new ImmutableMap.Builder<ArmorMaterial, StatusEffectInstance>()
+                    .put(ModArmorMaterials.BULWARK, new StatusEffectInstance(StatusEffects.HUNGER, 200, 1))
+                    .build();
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if(!world.isClient() && entity instanceof PlayerEntity player) {
+            if(hasFullSuitOfArmorOn(player)) {
+                evaluateArmorEffects(player);
+            }
+        }
+
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    private boolean hasFullSuitOfArmorOn(PlayerEntity player) {
+        ItemStack boots = player.getInventory().getArmorStack(0);
+        ItemStack leggings = player.getInventory().getArmorStack(1);
+        ItemStack chestplate = player.getInventory().getArmorStack(2);
+        ItemStack helmet = player.getInventory().getArmorStack(3);
+
+        return !boots.isEmpty() && !leggings.isEmpty()
+                && !chestplate.isEmpty() && !helmet.isEmpty();
+    }
+
+    private void evaluateArmorEffects(PlayerEntity player) {
+        for(Map.Entry<ArmorMaterial, StatusEffectInstance> entry : MATERIAL_TO_EFFECT_MAP1.entrySet()) {
+            ArmorMaterial mapArmorMaterial = entry.getKey();
+            StatusEffectInstance mapStatusEffect = entry.getValue();
+
+            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
+                addStatusEffectForMaterial(player, mapStatusEffect);
+                break;
+            }
+        }
+        for(Map.Entry<ArmorMaterial, StatusEffectInstance> entry : MATERIAL_TO_EFFECT_MAP2.entrySet()) {
+            ArmorMaterial mapArmorMaterial = entry.getKey();
+            StatusEffectInstance mapStatusEffect = entry.getValue();
+
+            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
+                addStatusEffectForMaterial(player, mapStatusEffect);
+                break;
+            }
+        }
+    }
+
+    private boolean hasCorrectArmorOn(ArmorMaterial mapArmorMaterial, PlayerEntity player) {
+        for(ItemStack armorStack : player.getArmorItems()) {
+            if(!(armorStack.getItem() instanceof ArmorItem)) {
+                return false;
+            }
+        }
+
+        ArmorItem boots = ((ArmorItem) player.getInventory().getArmorStack(0).getItem());
+        ArmorItem leggings = ((ArmorItem) player.getInventory().getArmorStack(1).getItem());
+        ArmorItem chestplate = ((ArmorItem) player.getInventory().getArmorStack(2).getItem());
+        ArmorItem helmet = ((ArmorItem) player.getInventory().getArmorStack(3).getItem());
+
+        return helmet.getMaterial() == mapArmorMaterial && chestplate.getMaterial() == mapArmorMaterial &&
+                leggings.getMaterial() == mapArmorMaterial && boots.getMaterial() == mapArmorMaterial;
+    }
+    private void addStatusEffectForMaterial(PlayerEntity player, StatusEffectInstance mapStatusEffect) {
+        boolean hasPlayerEffectAlready = player.hasStatusEffect(mapStatusEffect.getEffectType());
+
+        if(!hasPlayerEffectAlready) {
+            player.addStatusEffect(new StatusEffectInstance(mapStatusEffect.getEffectType(),
+                    mapStatusEffect.getDuration(), mapStatusEffect.getAmplifier()));
+        }
     }
 }
