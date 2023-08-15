8835cf03244955ec45abe6da2cd9b91593b4b077
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-08-15 15:27:10 -0400

    buffed waterborn regen but added poison effect

--- src/main/java/net/thev123/awesomearmaments/effect/Waterborn.java
+++ src/main/java/net/thev123/awesomearmaments/effect/Waterborn.java
@@ -6,19 +6,23 @@ import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
 
 import java.util.UUID;
 
 public class Waterborn extends StatusEffect {
     private int healingCooldown = 0;
-    private static final int MAX_COOLDOWN = 20; // 20 ticks (1 seconds) between heals
+    private static final int MAX_COOLDOWN = 12; // 12 ticks (.6 seconds) between regen
 
     private static final UUID HEALTH_MODIFIER_ID = UUID.fromString("f669e5a0-df49-11eb-ba80-0242ac130004");
     private static final double HEALTH_INCREASE_AMOUNT = 4.0; // 2 hearts increase (each heart is 2 health points)
 
     private static final UUID DAMAGE_REDUCTION_MODIFIER_ID = UUID.fromString("f669e5a1-df49-11eb-ba80-0242ac130004");
     private static final double DAMAGE_REDUCTION_AMOUNT = -3; //3 damage decrease while out of water
+    private static int timeOutsideWater = 0;
 
     protected Waterborn(StatusEffectCategory category, int color) {
         super(category, color);
@@ -28,6 +32,7 @@ public class Waterborn extends StatusEffect {
     public void applyUpdateEffect(LivingEntity pLivingEntity, int pAmplifier) {
         if (!pLivingEntity.getWorld().isClient()) {
             PlayerEntity player = (PlayerEntity) pLivingEntity;
+            World world = player.getWorld();
             EntityAttributeInstance attributeInstance = player.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
             EntityAttributeModifier healthModifier = new EntityAttributeModifier(
                     HEALTH_MODIFIER_ID,
@@ -44,6 +49,8 @@ public class Waterborn extends StatusEffect {
                     EntityAttributeModifier.Operation.ADDITION
             );
             if(player.isSubmergedInWater() || player.isSwimming() || player.isTouchingWater()){
+                timeOutsideWater = 0;
+                player.removeStatusEffect(StatusEffects.POISON);
                 if (attributeInstance != null && !attributeInstance.hasModifier(healthModifier)) {
                     attributeInstance.addTemporaryModifier(healthModifier);
                 }
@@ -66,6 +73,10 @@ public class Waterborn extends StatusEffect {
                 }
                 if(attributeInstance2 != null && !attributeInstance2.hasModifier(damageNerf)){
                     attributeInstance2.addTemporaryModifier(damageNerf);
+                }
+                timeOutsideWater++;
+                if(timeOutsideWater == 3600){
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 999999999, 0));
                 }
             }
         }
