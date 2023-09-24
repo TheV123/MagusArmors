f92007cb2cb6da3adf3a815607ee9d0723388118
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2023-09-24 18:51:03 -0400

    changed up order of creative mode tab items

--- src/main/java/net/thev123/awesomearmaments/item/ModItemGroups.java
+++ src/main/java/net/thev123/awesomearmaments/item/ModItemGroups.java
@@ -14,25 +14,6 @@ public class ModItemGroups {
             new Identifier(AwesomeArmaments.MOD_ID, "awesome_armaments"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.awesomearmaments"))
                     .icon(() -> new ItemStack(ModItems.INFINITE_SIGHT_CHESTPLATE)).entries((displayContext, entries) -> {
-                        entries.add(ModItems.PLATED_IRON_UPGRADE);
-                        entries.add(ModItems.PLATED_DIAMOND_UPGRADE);
-                        entries.add(ModItems.PLATED_NETHERITE_UPGRADE);
-
-                        entries.add(ModItems.WISE_IRON_UPGRADE);
-                        entries.add(ModItems.WISE_DIAMOND_UPGRADE);
-                        entries.add(ModItems.WISE_NETHERITE_UPGRADE);
-
-                        entries.add(ModItems.AQUA_DIAMOND_UPGRADE);
-                        entries.add(ModItems.AQUA_NETHERITE_UPGRADE);
-
-                        entries.add(ModItems.BERSERK_DIAMOND_UPGRADE);
-                        entries.add(ModItems.BERSERK_NETHERITE_UPGRADE);
-
-                        entries.add(ModItems.BULWARK_UPGRADE);
-                        entries.add(ModItems.BLOODLUST_UPGRADE);
-                        entries.add(ModItems.INFINITE_SIGHT_UPGRADE);
-                        entries.add(ModItems.NEPTUNE_UPGRADE);
-
                         entries.add(ModItems.WINDCALLER_UPGRADE);
                         entries.add(ModItems.WINDCALLER_SWORD);
                         entries.add(ModItems.WINDCALLER_AXE);
@@ -49,70 +30,89 @@ public class ModItemGroups {
                         entries.add(ModItems.WITHERING_SWORD);
                         entries.add(ModItems.WITHERING_AXE);
 
+                        entries.add(ModItems.PLATED_IRON_UPGRADE);
                         entries.add(ModItems.PLATED_IRON_HELMET);
                         entries.add(ModItems.PLATED_IRON_CHESTPLATE);
                         entries.add(ModItems.PLATED_IRON_LEGGINGS);
                         entries.add(ModItems.PLATED_IRON_BOOTS);
+
+                        entries.add(ModItems.PLATED_DIAMOND_UPGRADE);
                         entries.add(ModItems.PLATED_DIAMOND_HELMET);
                         entries.add(ModItems.PLATED_DIAMOND_CHESTPLATE);
                         entries.add(ModItems.PLATED_DIAMOND_LEGGINGS);
                         entries.add(ModItems.PLATED_DIAMOND_BOOTS);
+
+                        entries.add(ModItems.PLATED_NETHERITE_UPGRADE);
                         entries.add(ModItems.PLATED_NETHERITE_HELMET);
                         entries.add(ModItems.PLATED_NETHERITE_CHESTPLATE);
                         entries.add(ModItems.PLATED_NETHERITE_LEGGINGS);
                         entries.add(ModItems.PLATED_NETHERITE_BOOTS);
 
+                        entries.add(ModItems.WISE_IRON_UPGRADE);
                         entries.add(ModItems.WISE_IRON_HELMET);
                         entries.add(ModItems.WISE_IRON_CHESTPLATE);
                         entries.add(ModItems.WISE_IRON_LEGGINGS);
                         entries.add(ModItems.WISE_IRON_BOOTS);
+
+                        entries.add(ModItems.WISE_DIAMOND_UPGRADE);
                         entries.add(ModItems.WISE_DIAMOND_HELMET);
                         entries.add(ModItems.WISE_DIAMOND_CHESTPLATE);
                         entries.add(ModItems.WISE_DIAMOND_LEGGINGS);
                         entries.add(ModItems.WISE_DIAMOND_BOOTS);
+
+                        entries.add(ModItems.WISE_NETHERITE_UPGRADE);
                         entries.add(ModItems.WISE_NETHERITE_HELMET);
                         entries.add(ModItems.WISE_NETHERITE_CHESTPLATE);
                         entries.add(ModItems.WISE_NETHERITE_LEGGINGS);
                         entries.add(ModItems.WISE_NETHERITE_BOOTS);
 
+                        entries.add(ModItems.AQUA_DIAMOND_UPGRADE);
                         entries.add(ModItems.AQUA_DIAMOND_HELMET);
                         entries.add(ModItems.AQUA_DIAMOND_CHESTPLATE);
                         entries.add(ModItems.AQUA_DIAMOND_LEGGINGS);
                         entries.add(ModItems.AQUA_DIAMOND_BOOTS);
+
+                        entries.add(ModItems.AQUA_NETHERITE_UPGRADE);
                         entries.add(ModItems.AQUA_NETHERITE_HELMET);
                         entries.add(ModItems.AQUA_NETHERITE_CHESTPLATE);
                         entries.add(ModItems.AQUA_NETHERITE_LEGGINGS);
                         entries.add(ModItems.AQUA_NETHERITE_BOOTS);
 
+                        entries.add(ModItems.BERSERK_DIAMOND_UPGRADE);
                         entries.add(ModItems.BERSERK_DIAMOND_HELMET);
                         entries.add(ModItems.BERSERK_DIAMOND_CHESTPLATE);
                         entries.add(ModItems.BERSERK_DIAMOND_LEGGINGS);
                         entries.add(ModItems.BERSERK_DIAMOND_BOOTS);
+
+                        entries.add(ModItems.BERSERK_NETHERITE_UPGRADE);
                         entries.add(ModItems.BERSERK_NETHERITE_HELMET);
                         entries.add(ModItems.BERSERK_NETHERITE_CHESTPLATE);
                         entries.add(ModItems.BERSERK_NETHERITE_LEGGINGS);
                         entries.add(ModItems.BERSERK_NETHERITE_BOOTS);
 
+                        entries.add(ModItems.BULWARK_UPGRADE);
                         entries.add(ModItems.BULWARK_HELMET);
                         entries.add(ModItems.BULWARK_CHESTPLATE);
                         entries.add(ModItems.BULWARK_LEGGINGS);
                         entries.add(ModItems.BULWARK_BOOTS);
 
+                        entries.add(ModItems.INFINITE_SIGHT_UPGRADE);
                         entries.add(ModItems.INFINITE_SIGHT_HELMET);
                         entries.add(ModItems.INFINITE_SIGHT_CHESTPLATE);
                         entries.add(ModItems.INFINITE_SIGHT_LEGGINGS);
                         entries.add(ModItems.INFINITE_SIGHT_BOOTS);
 
+                        entries.add(ModItems.NEPTUNE_UPGRADE);
                         entries.add(ModItems.NEPTUNE_HELMET);
                         entries.add(ModItems.NEPTUNE_CHESTPLATE);
                         entries.add(ModItems.NEPTUNE_LEGGINGS);
                         entries.add(ModItems.NEPTUNE_BOOTS);
 
+                        entries.add(ModItems.BLOODLUST_UPGRADE);
                         entries.add(ModItems.BLOODLUST_HELMET);
                         entries.add(ModItems.BLOODLUST_CHESTPLATE);
                         entries.add(ModItems.BLOODLUST_LEGGINGS);
                         entries.add(ModItems.BLOODLUST_BOOTS);
-
 
                     }).build());
 
